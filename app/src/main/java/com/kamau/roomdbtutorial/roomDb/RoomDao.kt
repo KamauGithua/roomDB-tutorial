7eb42c47da7dd0e013afab156d4bd0c367258ca2
kamau <githuakamau99@gmail.com>
2024-09-13 15:28:16 +0300

    Added an RoomDao Interface File where I created Upsert, Delete and Query function

---
+++ app/src/main/java/com/kamau/roomdbtutorial/roomDb/RoomDao.kt
@@ -0,0 +1,17 @@
+package com.kamau.roomdbtutorial.roomDb
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface RoomDao {
+    @Upsert
+    suspend fun upsertNote(note: Note)
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    fun getAllNotes()
+}
